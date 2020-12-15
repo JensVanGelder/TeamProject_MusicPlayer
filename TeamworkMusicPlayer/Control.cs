f342cc189ffdf34a76ab1ac1ac7feb5117f8759f
JensVanGelder <jens.v.gelder@gmail.com>
2020-12-15 13:53:48 +0100

    Changed play/pause & mute/unmute to work in 1 button, Changed inputs to work without enter Layout changes

--- TeamworkMusicPlayer/Control.cs
+++ TeamworkMusicPlayer/Control.cs
@@ -9,8 +9,10 @@ namespace TeamworkMusicPlayer
     internal class Control
     {
         public string Path { get; set; }
-        public int Volume { get; set; }
+        public int Volume { get; set; } = 15;
         public string SongName { get; set; }
+        public bool IsPaused { get; set; }
+        public bool IsMuted { get; set; }
         private WindowsMediaPlayer player = new WindowsMediaPlayer();
 
         public void MainMenu()
@@ -50,7 +52,8 @@ namespace TeamworkMusicPlayer
             FileReaderWriter readerWriter = new FileReaderWriter();
             string logFile = "D:/VDAB_.net_C-/VDAB/Week3/TeamworkMusicPlayer/Log.txt";
             SongName = file.Substring(file.LastIndexOf("/")+1);
-            player.settings.volume = 5;
+            player.settings.volume = Volume;
+            IsPaused = false;
             player.URL = file;
             readerWriter.WriteDataTofile(SongName, logFile);
 
@@ -62,56 +65,91 @@ namespace TeamworkMusicPlayer
 
         public void UserInput()
         {
-            Console.WriteLine($"Currently playing : {SongName}\n");
+            ConsoleKeyInfo cki;
             Console.WriteLine("Controls: " +
-                "\n'pause', 'resume', 'mute', 'unmute', 'volume', 'newsong', 'stop', 'exit'");
-            string controlInput = Console.ReadLine();
-            switch (controlInput)
+                "\n1.Pause/Play" +
+                "\n2.Mute/Unmute" +
+                "\n3.Change volume" +
+                "\n4.Choose new song" +
+                "\n5.Stop playing song" +
+                "\n6.Quit player" +
+                "\n\n==================================================================" +
+                $"\n\nCurrently playing: {SongName}" +
+                $"\nVolume level {Volume}%");
+            cki = Console.ReadKey();
+
+            if (cki.Key == ConsoleKey.D1|| cki.Key == ConsoleKey.NumPad1)
             {
-                case "pause":
-                    player.controls.pause();
-                    break;
-
-                case "resume":
-                    player.controls.play();
-                    break;
-
-                case "mute":
-                    player.settings.mute = true;
-                    break;
-
-                case "unmute":
-                    player.settings.mute = false;
-                    break;
-
-                case "volume":
-                    SetVolume();
-                    break;
-
-                case "newsong":
-                    player.close();
-                    MainMenu();
-                    break;
-
-                case "stop":
-                    player.controls.stop();
-                    break;
-
-                case "exit":
-                    System.Environment.Exit(1);
-                    break;
-
-                default:
-                    Console.WriteLine("Error wrong input");
-                    Thread.Sleep(1000);
-                    break;
+                PausePlay();
+                Console.Clear();
+            }
+            else if (cki.Key == ConsoleKey.D2 || cki.Key == ConsoleKey.NumPad2)
+            {
+                MuteUnmute();
+                Console.Clear();
+            }
+            else if (cki.Key == ConsoleKey.D3 || cki.Key == ConsoleKey.NumPad3)
+            {
+                SetVolume();
+                Console.Clear();
+            }
+            else if (cki.Key == ConsoleKey.D4 || cki.Key == ConsoleKey.NumPad4)
+            {
+                player.close();
+                MainMenu();
+                Console.Clear();
+            }
+            else if (cki.Key == ConsoleKey.D5 || cki.Key == ConsoleKey.NumPad5)
+            {
+                player.controls.stop();
+                Console.Clear();
+            }
+            else if (cki.Key == ConsoleKey.D6 || cki.Key == ConsoleKey.NumPad6)
+            {
+                System.Environment.Exit(1);
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+                Thread.Sleep(1000);
+                Console.Clear();
+                UserInput();
             }
             Console.Clear();
         }
 
+        public void PausePlay()
+        {
+            if (IsPaused == true)
+            {
+                player.controls.play();
+                IsPaused = false;
+            }
+            else
+            {
+                player.controls.pause();
+                IsPaused = true;
+            }
+        }
+
+        public void MuteUnmute()
+        {
+            if (IsMuted== true)
+            {
+                player.settings.mute = false;
+                IsMuted = false;
+            }
+            else
+            {
+                player.settings.mute = true;
+                IsMuted = true;
+            }
+        }
+
         public void SetVolume()
         {
-            Console.WriteLine($"This is the current volume {Volume}" +
+            Console.WriteLine($"\nThis is the current volume {Volume}" +
                         "\nSelect volume between 1/100");
             Volume = Convert.ToInt32(Console.ReadLine());
             if (Volume > 100 || Volume < 0)
